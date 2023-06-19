ccbc840049e600ea2cc1597de17557f78a9070da
Tom Oketch <tomclement@users.noreply.github.com>
2023-06-19 16:43:54 -0700

    Apply PEP8 formatting

--- setup.py
+++ setup.py
@@ -1,24 +1,24 @@
 import setuptools
 
-with open("README.md", "r", encoding = "utf-8") as fh:
+with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name = "circlecigen",
-    author = "Nic Cheneweth",
-    author_email = "nchenewe@thoughtworks.com",
-    description = "Opinionated generation of continuation pipelines",
-    long_description = long_description,
-    long_description_content_type = "text/markdown",
-    url = "https://github.com/ThoughtWorks-DPS/circlecigen",
-    classifiers = [
+    name="circlecigen",
+    author="Nic Cheneweth",
+    author_email="nchenewe@thoughtworks.com",
+    description="Opinionated generation of continuation pipelines",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ThoughtWorks-DPS/circlecigen",
+    classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
     ],
-    package_dir = {"": "src"},
-    packages = setuptools.find_packages(where="src"),
-    python_requires = ">=3.10",
-    entry_points = '''
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.10",
+    entry_points='''
         [console_scripts]
         circlecigen=circlecigen:cli
     '''
